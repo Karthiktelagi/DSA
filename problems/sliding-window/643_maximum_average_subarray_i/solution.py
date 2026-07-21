adabc0935300d8302af846931b5170148e4e4478
Karthiktelagi <tskarthiktelagi@gmail.com>
2026-07-21 22:06:31 +0530

    Solve LeetCode 643 Maximum Average Subarray I

--- problems/sliding-window/643_maximum_average_subarray_i/solution.py
+++ problems/sliding-window/643_maximum_average_subarray_i/solution.py
@@ -1,11 +1,10 @@
 class Solution:
-    def findMaxAverage(self, nums: List[int], k: int) -> float:
-        sum = 0
-        for i in range(k):
-            sum += nums[i]
-        max = sum
+    def findMaxAverage(self, nums, k):
+        window_sum = sum(nums[:k])
+        max_sum = window_sum
+
         for i in range(k, len(nums)):
-            sum = sum + nums[i] - nums[i-k]
-            if sum > max:
-                max = sum
-        return max / k
+            window_sum += nums[i] - nums[i - k]
+            max_sum = max(max_sum, window_sum)
+
+        return max_sum / k
